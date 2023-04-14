edfc43d5083778e0fb88a7c787f9aea72e87fce4
tomas <tomasreuquepy@gmail.com>
2023-04-14 18:05:44 -0300

    commit host

--- app.py
+++ app.py
@@ -52,8 +52,7 @@ def get_count_platform(platform: str):
     return count
 
 
-
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=10000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
